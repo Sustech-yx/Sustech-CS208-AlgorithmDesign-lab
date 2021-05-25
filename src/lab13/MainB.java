aaad569b2f300d8e11766cd22bfbde6c556dfc97
Sustech-yx <59305237+Sustech-yx@users.noreply.github.com>
2021-05-26 01:54:40 +0800

    put the structure

---
+++ src/lab13/MainB.java
@@ -0,0 +1,79 @@
+package lab13;
+
+import java.io.*;
+import java.util.*;
+
+public class MainB {
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+        public boolean hasNext() {
+            try {
+                String string = reader.readLine();
+                if (string == null) {
+                    return false;
+                }
+                tokenizer = new StringTokenizer(string);
+                return tokenizer.hasMoreTokens();
+            } catch (IOException e) {
+                return false;
+            }
+        }
+    }
+    static InputReader in = new InputReader(System.in);
+    static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) {
+        Run r = new Run();
+        int t = in.nextInt();
+        r.start();
+        ini();
+        while (t -- > 0) {
+            solve();
+        }
+        r.end();
+        out.close();
+    }
+    static void ini () {
+
+    }
+    static void solve() {
+
+    }
+    static class Run {
+        long startTime;
+        long endTime;
+        public Run () {
+            start();
+        }
+
+        public void start () {
+            startTime = System.currentTimeMillis();
+        }
+        public void end () {
+            endTime = System.currentTimeMillis();
+            out.println("\n" + (endTime - startTime) + "ms");
+        }
+    }
+}
+
