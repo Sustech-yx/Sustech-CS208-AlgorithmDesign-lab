5432635bb506cba276ebb930876872532cdaa0d7
Sustech-yx <59305237+Sustech-yx@users.noreply.github.com>
2021-05-27 11:48:56 +0800

    现在在做B

--- src/lab13/MainB.java
+++ src/lab13/MainB.java
@@ -54,10 +54,26 @@ public class MainB {
         r.end();
         out.close();
     }
+    static long[] pow;
     static void ini () {
-
+        pow = new long[55];
+        pow[0] = 1;
+        for (int i = 1; i < 55; i++) {
+            pow[i] = pow[i-1] * 2;
+        }
     }
     static void solve() {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextLong();
+        }
+        long[] preSum = new long[n];
+        preSum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            preSum[i] = preSum[i-1] + arr[i];
+        }
 
     }
     static class Run {
